7ea0c0e8b6dcade7c305e56cb9fb94b16421eb6a
Philippe <92359903+phylxs@users.noreply.github.com>
2022-04-19 09:36:23 +0200

    erreur details

--- pages/products/[permalink].js
+++ pages/products/[permalink].js
@@ -63,55 +63,7 @@ export default function ProductPage({ product, categories, products }) {
           </div>
           <div className=" ">
             <div className="">
-              <h1>{product.name}</h1>
-              <p>{product.description}</p>
-            <fieldset>
-            <legend>{product.variant_groups[0].name}</legend>
-
-            
-                <div className="flex items-center mb-4">
-                  <input type="radio" name="sizes" value={`${product.variant_groups[0].options[0].name}`}
-                    className="w-4 h-4 border-gray-300 focus:ring-2 focus:ring-blue-300 dark:focus:ring-blue-600
-                   dark:focus:bg-blue-600 dark:bg-gray-700 dark:border-gray-600" aria-labelledby="size-1"
-                    aria-describedby="country-option-1" />
-                  <label htmlFor="size-s" className="block ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-                    {product.variant_groups[0].options[0].name}
-                  </label>
-                </div>
-
-                <div className="flex items-center mb-4">
-                  <input type="radio" name="sizes" value={`${product.variant_groups[0].options[0].name}`}
-                    className="w-4 h-4 border-gray-300 focus:ring-2 focus:ring-blue-300 dark:focus:ring-blue-600
-                   dark:focus:bg-blue-600 dark:bg-gray-700 dark:border-gray-600" aria-labelledby="size-1"
-                    aria-describedby="country-option-1" defaultChecked />
-                  <label htmlFor="size-s" className="block ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-                    {product.variant_groups[0].options[1].name}
-                  </label>
-                </div>
-
-                <div className="flex items-center mb-4">
-                  <input type="radio" name="sizes" value={`${product.variant_groups[0].options[0].name}`}
-                    className="w-4 h-4 border-gray-300 focus:ring-2 focus:ring-blue-300 dark:focus:ring-blue-600
-                   dark:focus:bg-blue-600 dark:bg-gray-700 dark:border-gray-600" aria-labelledby="size-1"
-                    aria-describedby="country-option-1" />
-                  <label htmlFor="size-s" className="block ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-                    {product.variant_groups[0].options[2].name}
-                  </label>
-                </div>
-
-                <div className="flex items-center mb-4">
-                  <input type="radio" name="sizes" value={`${product.variant_groups[0].options[0].name}`}
-                    className="w-4 h-4 border-gray-300 focus:ring-2 focus:ring-blue-300 dark:focus:ring-blue-600
-                   dark:focus:bg-blue-600 dark:bg-gray-700 dark:border-gray-600" aria-labelledby="size-1"
-                    aria-describedby="country-option-1" />
-                  <label htmlFor="size-s" className="block ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-                    {product.variant_groups[0].options[3].name}
-                  </label>
-                </div>
-           
-
-              </fieldset> 
-              <p>{product.price.formatted_with_symbol}</p>
+              
               <button className=""><a>Add to cart</a></button>
             </div>
           </div>
