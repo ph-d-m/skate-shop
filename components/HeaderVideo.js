d77011c7f9e83882d33e5b0452715517b562ccf0
Philippe <92359903+phylxs@users.noreply.github.com>
2022-07-06 11:28:07 +0200

    del comments

--- components/HeaderVideo.js
+++ components/HeaderVideo.js
@@ -1,5 +1,4 @@
-import Marquee from './Marquee'
-import Image from 'next/image'
+
 import styles from './modules/Video.module.css'
 
 export default function HeaderVideo({children}) {
@@ -10,10 +9,6 @@ export default function HeaderVideo({children}) {
                <source src="jaajskateboard-small.webm" type="video/webm" />
                Sorry, your browser doesn't support embedded videos.
              </video>
-                {/* <div className="buttonVideo">
-                    <Image src="/img/Pause.svg" width={28} height={28} alt="Play/Pause" />
-                </div> 
-             <Marquee />*/}
         </section>
     )
 }
